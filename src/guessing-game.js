2e48db176d0c188d1de44648ca7d1a1a6d7b7031
gru6 <iliagrushevich@gmail.com>
2022-08-12 00:27:45 +0300

    feat: guessing-game done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number) {
+        this.number = number;
+    }
 
     setRange(min, max) {
-
+        this.min = min; //0
+        this.max = max; //4048
     }
 
     guess() {
-
+        this.mid = Math.ceil((this.min + this.max) / 2);
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
+
 
 module.exports = GuessingGame;
